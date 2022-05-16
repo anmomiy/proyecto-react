4b35be91edb8bcfab6b31b1161679e3f0079eeb8
anmomiy <an.momiy@gmail.com>
2022-05-15 19:00:29 -0500

    navbar y favicon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <h1>Proyecto de React</h1>
       <img src="https://placekitten.com/800/600"></img>
     </div>
