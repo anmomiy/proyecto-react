cd148e1400a834f8dca378ff1888877ec2f0af33
anmomiy <an.momiy@gmail.com>
2022-05-18 19:22:12 -0500

    cartwidget

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,9 @@
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import './CartWidget.css'
+const CartWidget = () =>{
+    return(
+        <ShoppingCartIcon/>
+    )
+}
+
+export default CartWidget;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -9,7 +9,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import CartWidget from '../CartWidget/CartWidget'
 import './NavBar.css';
 const pages = ['Nosotros', 'Productos', 'Contacto'];
 
@@ -115,7 +115,7 @@ const NavBar = () => {
             ))}
           </Box>
 
-          <ShoppingCartIcon/>
+          <CartWidget/>
         </Toolbar>
       </Container>
     </AppBar>
